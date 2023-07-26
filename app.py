a2d1cfea98ec479dc01e8d81126e2b7fcd451f25
daffodills-02 <140621611+daffodills-02@users.noreply.github.com>
2023-07-26 18:56:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-# Bring in deps
-import streamlit as st 
-from langchain.llms import LlamaCpp
-from langchain.embeddings import LlamaCppEmbeddings
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
 
+import streamlit as st
+from dotenv import find_dotenv, load_dotenv
+from src.utils import setup_dbqa
+from db_build import run_db_build
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.base import CallbackManager
+
+def generate_response(uploaded_file, query_text):
+    if uploaded_file is not None:
+        run_db_build(uploaded_file)
+        dbqa = setup_dbqa()
+        return dbqa.run(query_text)
 
 # Customize the layout
-st.set_page_config(page_title="DOCAI", page_icon="🤖", layout="wide", )     
+st.set_page_config(page_title="PnC Claims", page_icon="🤖", layout="wide", )     
 st.markdown(f"""
             <style>
             .stApp {{background-image: url("https://images.unsplash.com/photo-1509537257950-20f875b03669?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80"); 
@@ -18,52 +22,25 @@ st.markdown(f"""
                      background-size: cover}}
          </style>
          """, unsafe_allow_html=True)
+st.title("📄 Analyse Claim 🤖")
 
-# function for writing uploaded file in temp
-def write_text_file(content, file_path):
-    try:
-        with open(file_path, 'w') as file:
-            file.write(content)
-        return True
-    except Exception as e:
-        print(f"Error occurred while writing the file: {e}")
-        return False
 
-# set prompt template
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+# File upload
+uploaded_file = st.file_uploader('Upload an article', type='pdf')
+# Query text
+query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
-{context}
 
-Question: {question}
-Answer:"""
-prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+# Form input and query
+result = []
+with st.form('myform', clear_on_submit=True):
+    # openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+    if submitted :
+        with st.spinner('Calculating...'):
+            response = generate_response(uploaded_file,  query_text)
+            result.append(response)
+            
 
-# initialize the LLM & Embeddings
-llm = LlamaCpp(model_path="./models/llama-7b.ggmlv3.q4_0.bin")
-embeddings = LlamaCppEmbeddings(model_path="models/llama-7b.ggmlv3.q4_0.bin")
-llm_chain = LLMChain(llm=llm, prompt=prompt)
-
-st.title("📄 Document Conversation 🤖")
-uploaded_file = st.file_uploader("Upload an article", type="txt")
-
-if uploaded_file is not None:
-    content = uploaded_file.read().decode('utf-8')
-    # st.write(content)
-    file_path = "temp/file.txt"
-    write_text_file(content, file_path)   
-    
-    loader = TextLoader(file_path)
-    docs = loader.load()    
-    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-    texts = text_splitter.split_documents(docs)
-    db = Chroma.from_documents(texts, embeddings)    
-    st.success("File Loaded Successfully!!")
-    
-    # Query through LLM    
-    question = st.text_input("Ask something from the file", placeholder="Find something similar to: ....this.... in the text?", disabled=not uploaded_file,)    
-    if question:
-        similar_doc = db.similarity_search(question, k=1)
-        context = similar_doc[0].page_content
-        query_llm = LLMChain(llm=llm, prompt=prompt)
-        response = query_llm.run({"context": context, "question": question})        
-        st.write(response)
+if len(result):
+    st.info(response)
